9b94dac69865c17d59e524d8fc77c64f55237abf
tashiro.hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2018-04-29 21:15:49 +0900

    modified hello.js

--- routes/hello.js
+++ routes/hello.js
@@ -12,5 +12,13 @@ router.post('/', function (req, res) {
     res.send('Got a POST request. POST Data:: name:' + req.body.name);
 });
 
+router.put('/user', function (req, res) {
+  res.send('Got a PUT request at /hello');
+});
+
+router.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /hello');
+});
+
 
 module.exports = router;
