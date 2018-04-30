f89ba9430df3c7ef076bb1f0d7bb7f7edd4e71cc
tashiro.hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2018-04-30 19:47:09 +0900

    create data or find

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var models = require("../models");
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var user_id = req.query.user_id;
+  var firstName = req.query.first_name;
+  var lastName = req.query.last_name;
+  models.User.findOrCreate(
+    {"where": {"id": user_id},
+     "defaults": {"id": user_id, "firstName": firstName, "lastName": lastName}}
+  ).spread(function(user, created){
+    res.send({"user": user, "created": created});
+  });
 });
 
 module.exports = router;
