d2da24123d8ed134493c8ea5794a42f4afc2fd60
tashiro.hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2018-04-30 18:55:19 +0900

    use mysql

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # macheb
 
+$ sudo apt-get install clipit
+
+
+````
 npm init
 npm install express --save
 $ npm install express-generator -g
@@ -7,3 +11,22 @@ $ express --view=pug macheb
 $ cd macheb
 $ npm install
 $ DEBUG=macheb:* npm start
+
+$ sudo apt-get update
+$ sudo apt-get install mysql-server
+$ mysql --version
+mysql  Ver 14.14 Distrib 5.7.21, for Linux (x86_64) using  EditLine wrapper
+
+$ npm install mysql
+
+$ mysql -u root -p
+
+mysql> set password for root@localhost=password('[password]');
+
+````
+
+https://obel.hatenablog.jp/entry/20170823/1503479469
+
+https://qiita.com/ksugawara61/items/d8d69b7d57a3afcef980
+
+https://qiita.com/RyochanUedasan/items/9a49309019475536d22a
--- routes/hello.js
+++ routes/hello.js
@@ -3,13 +3,45 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql      = require('mysql');
+
+const Sequelize = require('sequelize');
+
 /* GET hello listing. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'Hello' });
+
+  //http://docs.sequelizejs.com/manual/index.html
+  const seq = new Sequelize('macheb', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+  var User = seq.define('User', {
+  name: Sequelize.STRING
 });
+seq.sync(function(errs)
+{
+  //console.log('DATABASE SYNC', errs);
+});
+var newUser = new User({
+    name: 'user2'
+});
+
+newUser.save();
+  res.render('hello', { title: 'Hello' });
+  // var connection = mysql.createConnection({
+  //   host     : 'localhost',
+  //   user     : 'root',
+  //   password : ''
+  // });
+  //
+  // //これは省略してもoK。
+  // connection.connect();
+});
+
 
 router.post('/', function (req, res) {
     res.send('Got a POST request. POST Data:: name:' + req.body.name);
+
 });
 
 router.put('/user', function (req, res) {
